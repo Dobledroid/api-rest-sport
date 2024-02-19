edbd6370b72646e1c95c2be45a4ff00c09e3eae5
Dobledroid <carlosexequiel360@gmail.com>
2024-02-18 21:05:04 -0600

    lptm

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import app from "./app";
+
+// app.listen(app.get("port"));
+
+// console.log("Server on port", app.get("port"));
+import { app } from "./app";
+
+const PORT = process.env.PORT || 3001; // Puerto por defecto 3001
+
+app.listen(PORT, () => {
+  console.log(`Servidor en el puerto ${PORT}`);
+});
